3642079c7aa0e376b43fb207dfeb788f02d4ce0e
Pro Chivs <prochivia@gmail.com>
2023-02-20 00:24:54 +0200

    updates

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+
+app.get("/", (req, res) => {
+  return res.json("Talking Avatar API V1.0.");
+  // return res.json({ token: token })
+});
+app.use('/user', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
